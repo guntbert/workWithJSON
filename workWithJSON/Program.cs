76ceb3678224314cca85c4bbdcb4e22ad3876d8c
Guntbert Reiter <re@htlweiz.at>
2018-06-05 11:44:10 +0200

    Serialze/Deserialize array of objects

--- workWithJSON/Program.cs
+++ workWithJSON/Program.cs
@@ -29,19 +29,56 @@ namespace workWithJSON
                 }
             }
             };
-            
+
             // Serializing Class->JSON
-            string jsonBill=JsonConvert.SerializeObject(bill);
+            string jsonBill = JsonConvert.SerializeObject(bill);
             Console.WriteLine(jsonBill);
 
             Customer newCust = JsonConvert.DeserializeObject<Customer>(jsonBill);
             Console.WriteLine($"Name:{newCust.Name}, Id:{newCust.Id}");
             foreach (PhoneNumber pn in newCust.phoneNumbers)
             {
-                Console.WriteLine(pn.AreaCode+pn.number);
+                Console.WriteLine(pn.AreaCode + pn.number);
             }
 
+            List<Customer> allCustomers = new List<Customer>();
+            Customer anotherCust = new Customer
+            {
+                Id = 2,
+                Name = "George Washington",
+                phoneNumbers = new List<PhoneNumber>
+                {
+                    new PhoneNumber
+                    {
+                    AreaCode="WAS",
+                    number="01010101"
+                    }
+                }
+            };
+
+            allCustomers.Add(anotherCust);
+            anotherCust = new Customer
+            {
+                Id = 3,
+                Name = "Mariah Carey",
+                phoneNumbers = new List<PhoneNumber>
+                {
+                    new PhoneNumber
+                    {
+                    AreaCode="HOL",
+                    number="56566565"
+                    }
+                }
+            };
+            allCustomers.Add(anotherCust);
+
+            string jsonAll = JsonConvert.SerializeObject(allCustomers);
+            Console.WriteLine(jsonAll);
+            List<Customer> newCustList = JsonConvert.DeserializeObject<List<Customer>>(jsonAll);
 
         }
+
+
     }
 }
+
