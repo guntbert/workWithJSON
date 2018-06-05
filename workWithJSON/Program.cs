bf83654ec36bd94e9ef322ddd0fe213f1c194b92
Guntbert Reiter <re@htlweiz.at>
2018-06-05 10:58:40 +0200

    Serialize/deserialize single Object

--- workWithJSON/Program.cs
+++ workWithJSON/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace workWithJSON
 {
@@ -10,6 +11,37 @@ namespace workWithJSON
     {
         static void Main(string[] args)
         {
+            Customer bill = new Customer
+            {
+                Id = 1,
+                Name = "Bill Gates",
+                phoneNumbers = new List<PhoneNumber>
+            {
+                new PhoneNumber
+                {
+                    AreaCode="CAL",
+                    number="122356"
+                },
+                new PhoneNumber
+                {
+                    AreaCode="Wash",
+                    number="563498"
+                }
+            }
+            };
+            
+            // Serializing Class->JSON
+            string jsonBill=JsonConvert.SerializeObject(bill);
+            Console.WriteLine(jsonBill);
+
+            Customer newCust = JsonConvert.DeserializeObject<Customer>(jsonBill);
+            Console.WriteLine($"Name:{newCust.Name}, Id:{newCust.Id}");
+            foreach (PhoneNumber pn in newCust.phoneNumbers)
+            {
+                Console.WriteLine(pn.AreaCode+pn.number);
+            }
+
+
         }
     }
 }
